e9bfb74ddec6dfa63cc13ee3f687d1fdfcab74c4
AreTea <tbusinessr@gmail.com>
2019-07-19 11:10:47 -0400

    debugging image issues

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,10 @@ export default {
 </script>
 
 <style>
+.home-hero {
+    background: url('http://source.unsplash.com/0BhSKStVtdM');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
 </style>
